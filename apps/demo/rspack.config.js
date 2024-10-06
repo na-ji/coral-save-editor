b5a25852289b6de999eb2bda64ceef553be79842
Naji Astier <na-ji@users.noreply.github.com>
2024-10-06 20:20:53 +0200

    build(demo): correctly serve app from subfolder

--- apps/demo/rspack.config.js
+++ apps/demo/rspack.config.js
@@ -2,5 +2,10 @@ const { composePlugins, withNx, withReact } = require('@nx/rspack');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   config.experiments.asyncWebAssembly = true;
+  config.output.publicPath = '/coral-save-editor/';
+  if (!config.devServer.devMiddleware) {
+    config.devServer.devMiddleware = {};
+  }
+  config.devServer.devMiddleware.publicPath = '/coral-save-editor/';
   return config;
 });
