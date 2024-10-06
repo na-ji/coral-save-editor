10192e6d7b1a321af99c0f0bac13794c5094ac17
Naji Astier <na-ji@users.noreply.github.com>
2024-10-06 19:42:56 +0200

    feat(demo): add save export action

--- apps/demo/src/app/app.tsx
+++ apps/demo/src/app/app.tsx
@@ -1,11 +1,34 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import { decode_save } from '@coral/save-parser';
+import { decode_save, encode_save } from '@coral/save-parser';
 import { ChangeEvent, useCallback, useState } from 'react';
 import { SvelteJSONEditor } from './JSONEditor';
 
+const downloadURL = (url: string, fileName: string) => {
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.style.display = 'none';
+  a.click();
+  a.remove();
+};
+
+const downloadBlob = (data: Uint8Array, fileName: string, mimeType: string) => {
+  const blob = new Blob([data], {
+    type: mimeType,
+  });
+
+  const url = window.URL.createObjectURL(blob);
+
+  downloadURL(url, fileName);
+
+  setTimeout(() => window.URL.revokeObjectURL(url), 1000);
+};
+
 export function App() {
-  const [saveData, setSaveData] = useState<{ json: object } | undefined>();
+  const [fileContent, setFileContent] = useState<{ name: string; content: ArrayBuffer } | undefined>();
+  const [saveData, setSaveData] = useState<{ json: object; text: undefined } | undefined>();
 
   const handleFileSelection = useCallback((changeEvent: ChangeEvent<HTMLInputElement>) => {
     const saveFile = changeEvent.target.files[0];
@@ -13,7 +36,8 @@ export function App() {
     reader.addEventListener('loadend', (event) => {
       try {
         const binarySave = decode_save(event.target.result);
-        setSaveData({ json: binarySave });
+        setSaveData({ json: binarySave, text: undefined });
+        setFileContent({ name: saveFile.name, content: event.target.result });
       } catch (e) {
         console.error(e);
       }
@@ -22,10 +46,24 @@ export function App() {
     reader.readAsArrayBuffer(saveFile);
   }, []);
 
+  const exportSave = useCallback(() => {
+    if (!fileContent || !saveData) {
+      return;
+    }
+
+    const fileData = encode_save(fileContent.content, saveData.json);
+    downloadBlob(fileData, fileContent.name, 'application/octet-stream');
+  }, [fileContent, saveData]);
+
   return (
     <div>
-      <input id="save" type="file" onChange={handleFileSelection} />
-      {saveData && <SvelteJSONEditor content={saveData} readOnly={true} />}
+      <input id="save" type="file" onChange={handleFileSelection} style={{ marginBottom: '1rem' }} />
+      {saveData && (
+        <>
+          <button onClick={exportSave}>Export</button>
+          <SvelteJSONEditor content={saveData} onChange={setSaveData} />
+        </>
+      )}
     </div>
   );
 }
