ef6af518cfaa38c12aaea83ca832e3c5e3d89120
Alexander Niersmann <aniersmann@gmail.com>
2024-10-12 19:32:06 +0200

    feat(editor): added drag n drop to file selection

--- apps/editor/src/app/file-handler/file-handler.component.ts
+++ apps/editor/src/app/file-handler/file-handler.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SaveGameService } from '../core/save-game/save-game.service';
 
@@ -7,9 +7,15 @@ import { SaveGameService } from '../core/save-game/save-game.service';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './file-handler.component.html',
-  styleUrl: './file-handler.component.css',
+  styleUrl: './file-handler.component.scss',
+  host: {
+    '[class.is-dragging-over-body]': 'isDraggingOverBody()',
+    '[class.is-dragging-over]': 'isDraggingOver()',
+  },
 })
 export class FileHandlerComponent {
+  isDraggingOverBody = signal<boolean>(false);
+  isDraggingOver = signal<boolean>(false);
   #saveGameService = inject(SaveGameService);
 
   parseSaveGame($event: Event) {
@@ -18,5 +24,53 @@ export class FileHandlerComponent {
     const file = files?.item(0);
 
     if (file) this.#saveGameService.parseSaveGame(file);
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.isDraggingOver.set(false);
+
+    const { dataTransfer } = event;
+
+    if (!dataTransfer) return;
+
+    if (dataTransfer.items) {
+      const files = [];
+      for (let i = 0; i < dataTransfer.items.length; i++) {
+        if (dataTransfer.items[i].kind === 'file') {
+          files.push(dataTransfer.items[i].getAsFile());
+        }
+      }
+      dataTransfer.items.clear();
+      if (files.length === 1) this.#saveGameService.parseSaveGame(files[0]!);
+    } else {
+      const files = dataTransfer.files;
+      dataTransfer.clearData();
+      if (files.length === 1) this.#saveGameService.parseSaveGame(files[0]!);
+    }
+  }
+
+  @HostListener('dragover')
+  onDragOver() {
+    this.isDraggingOver.set(true);
+  }
+
+  @HostListener('dragleave')
+  onDragLeave() {
+    this.isDraggingOver.set(false);
+  }
+
+  @HostListener('body:dragover', ['$event'])
+  onBodyDragOver($event: DragEvent) {
+    $event.preventDefault();
+    this.isDraggingOverBody.set(true);
+  }
+
+  @HostListener('body:drop', ['$event'])
+  @HostListener('body:dragleave', ['$event'])
+  onBodyDragLeaveDrop($event: DragEvent) {
+    $event.preventDefault();
+    this.isDraggingOverBody.set(false);
   }
 }
