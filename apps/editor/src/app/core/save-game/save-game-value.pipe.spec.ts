fbaf1ca0e8d9e874ce1364a6b9b200f68d457d0d
Alexander Niersmann <aniersmann@gmail.com>
2024-10-12 13:58:09 +0200

    refactor(editor): removed spec file

--- apps/editor/src/app/core/save-game/save-game-value.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SaveGameValuePipe } from './save-game-value.pipe';
-
-describe('SaveGameValuePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SaveGameValuePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
