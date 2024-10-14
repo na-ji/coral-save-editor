5a16d8b9e76f636e9171f0f5b56fd216b861fd08
Alexander Niersmann <aniersmann@gmail.com>
2024-10-14 18:52:49 +0200

    chore(editor): installed material

--- apps/editor/src/app/app.config.ts
+++ apps/editor/src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes, withComponentInputBinding())],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideAnimationsAsync(),
+  ],
 };
