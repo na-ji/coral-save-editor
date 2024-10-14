8e0f59b33c4ccfca65d6dfe4084f38e76d9ddfb7
Alexander Niersmann <aniersmann@gmail.com>
2024-10-15 01:22:20 +0200

    feat(editor): added player gender

--- apps/editor/src/app/coral-island-editor/player/player-info/player-info.component.ts
+++ apps/editor/src/app/coral-island-editor/player/player-info/player-info.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { PrimitiveFormPartComponent } from '../../../form-parts/primitive-form-part/primitive-form-part.component';
+import { EnumFormComponent } from '../../forms/enum-form/enum-form.component';
 
 @Component({
   selector: 'app-player-info',
   standalone: true,
-  imports: [PrimitiveFormPartComponent],
+  imports: [PrimitiveFormPartComponent, EnumFormComponent],
   templateUrl: './player-info.component.html',
   styleUrl: './player-info.component.scss',
 })
