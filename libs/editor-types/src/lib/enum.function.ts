9fb28e4ff6507d2f7640762244e79f06da60ccb9
Alexander Niersmann <aniersmann@gmail.com>
2024-10-15 01:48:48 +0200

    refactor(editor): allow already prefixed enums

--- libs/editor-types/src/lib/enum.function.ts
+++ libs/editor-types/src/lib/enum.function.ts
@@ -1,7 +1,8 @@
 export function editorEnum<TType extends string, TValue extends string>(type: TType, value: TValue) {
+  const enumValue = value.startsWith(type) ? value : `${type}::${value}`;
   return {
     Enum: {
-      value: `${type}::${value}`,
+      value: enumValue,
       enum_type: `${type}`,
     },
   } as const;
