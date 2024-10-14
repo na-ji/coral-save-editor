594b5b59c867f8b160ce822673b08be1161e6925
Alexander Niersmann <aniersmann@gmail.com>
2024-10-15 01:34:05 +0200

    feat(editor): added current weather

--- apps/editor/src/app/coral-island-editor/world/world.component.ts
+++ apps/editor/src/app/coral-island-editor/world/world.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DateFormComponent } from '../forms/date-form/date-form.component';
 import { CardComponent } from '@coral-island/ui';
+import { EnumFormComponent } from '../forms/enum-form/enum-form.component';
 
 @Component({
   selector: 'app-world',
   standalone: true,
-  imports: [DateFormComponent, CardComponent],
+  imports: [DateFormComponent, CardComponent, EnumFormComponent],
   templateUrl: './world.component.html',
   styleUrl: './world.component.scss',
 })
