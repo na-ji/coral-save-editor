6665b9f694de98ad9767563ffb8e06a4522119f1
Alexander Niersmann <aniersmann@gmail.com>
2024-10-21 19:19:14 +0200

    refactor(editor): added missing http provider

--- apps/editor/src/app/app.config.ts
+++ apps/editor/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes, withComponentInputBinding()),
     provideAnimationsAsync(),
